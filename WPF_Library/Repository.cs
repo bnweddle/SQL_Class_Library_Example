17d1e6a9270d36f6e89c6e19df589f92121f7e70
Bethany Weddle <bnweddle@ksu.edu>
2021-03-09 20:19:03 -0600

    Repository Class

---
+++ WPF_Library/Repository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_Library
+{
+    // This is where we will store all the procedure and view calls
+    public class Repository
+    {
+
+    }
+}
